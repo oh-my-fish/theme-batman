fe686f239d428b3a4223def4a309b51d623cc1ba
Dylan Maccora <maccora17@gmail.com>
2020-01-07 22:25:33 -0800

    Fix issue when detached head

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -13,7 +13,11 @@ function git::get_behind_count
 end
 
 function git::branch_name
-  command git symbolic-ref --short HEAD
+  if git symbolic-ref --short HEAD 2> /dev/null
+    echo (command git symbolic-ref --short HEAD 2> /dev/null)
+  else
+    echo (command git rev-parse --short HEAD)
+  end
 end
 
 function git::is_touched
